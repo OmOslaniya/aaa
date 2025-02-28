0e35152bb58edad5707c95345b2cc36f2a0cf1ef
deeposlaniya93@gmail.com <deeposlaniya93@gmail.com>
2025-02-28 04:19:16 +0000

    python model

---
+++ models/example/demo.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import snowflake.snowpark.functions as F
+from snowflake.snowpark.session import Session
+
+def model(dbt, session: Session):
+    # Get source table
+    df = dbt.ref("SALES_US")
+
+    # Perform transformation: Multiply QUANTITY_SOLD by 2
+    df = df.with_column("QUANTITY_SOLD_DOUBLED", df["QUANTITY_SOLD"] * 2)
+
+    return df
